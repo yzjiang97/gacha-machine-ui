b15d0ca9b0b0716fec98f4b8a0dd5177dabc6010
江雨 <jiangyuzhen97@gmail.com>
2025-06-12 10:21:49 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+}
+</script>
+
+<style>
+
+html,
+body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+
+</style>
